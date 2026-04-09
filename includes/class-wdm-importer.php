a00e07e96f057357c311253964573bc3f8ecbfba
chomusuke-png <owokenji@gmail.com>
2026-04-09 16:06:14 -0400

    taxonomias

--- includes/class-wdm-importer.php
+++ includes/class-wdm-importer.php
@@ -24,6 +24,7 @@ class WDM_Importer {
         $total_pages = $response['total_pages'];
         $skipped     = array();
         $imported    = 0;
+        
         $parsed_old_url = wp_parse_url( $url );
         $old_domain     = isset( $parsed_old_url['host'] ) ? $parsed_old_url['host'] : '';
 
@@ -39,15 +40,13 @@ class WDM_Importer {
             $post_content = wp_kses_post( $post_data['content']['rendered'] );
             $post_date    = sanitize_text_field( $post_data['date'] );
 
-            $new_post_args = array(
+            $new_post_id = wp_insert_post( array(
                 'post_title'   => $post_title,
-                'post_content' => '',
+                'post_content' => '', 
                 'post_status'  => 'publish',
                 'post_type'    => 'post',
                 'post_date'    => $post_date,
-            );
-
-            $new_post_id = wp_insert_post( $new_post_args, true );
+            ), true );
 
             if ( is_wp_error( $new_post_id ) ) {
                 $skipped[] = "Error al insertar Post '{$post_title}': " . $new_post_id->get_error_message();
@@ -55,6 +54,10 @@ class WDM_Importer {
             }
 
             update_post_meta( $new_post_id, '_wdm_original_post_id', $original_id );
+
+            if ( isset( $post_data['_embedded']['wp:term'] ) ) {
+                $this->process_taxonomies( $new_post_id, $post_data['_embedded']['wp:term'] );
+            }
 
             if ( isset( $post_data['_embedded']['wp:featuredmedia'][0]['source_url'] ) ) {
                 $image_url = esc_url_raw( $post_data['_embedded']['wp:featuredmedia'][0]['source_url'] );
@@ -98,14 +101,36 @@ class WDM_Importer {
             'posts_per_page' => 1,
             'no_found_rows'  => true,
         ) );
-        
         return ! empty( $query->posts );
     }
 
-    private function process_inline_images( $content, $post_id, $old_domain ) {
-        if ( empty( $content ) || ! is_string( $content ) ) {
-            return (string) $content;
+    private function process_taxonomies( $post_id, $terms_groups ) {
+        foreach ( $terms_groups as $group ) {
+            foreach ( $group as $term ) {
+                $taxonomy = ( $term['taxonomy'] === 'post_tag' ) ? 'post_tag' : 'category';
+                
+                $local_term = get_term_by( 'slug', $term['slug'], $taxonomy );
+
+                if ( ! $local_term ) {
+                    $inserted = wp_insert_term( $term['name'], $taxonomy, array(
+                        'slug'        => $term['slug'],
+                        'description' => $term['description'] ?? ''
+                    ) );
+                    
+                    $term_id = ( ! is_wp_error( $inserted ) ) ? $inserted['term_id'] : false;
+                } else {
+                    $term_id = $local_term->term_id;
+                }
+
+                if ( $term_id ) {
+                    wp_set_object_terms( $post_id, (int) $term_id, $taxonomy, true );
+                }
+            }
         }
+    }
+
+    private function process_inline_images( $content, $post_id, $old_domain ) {
+        if ( empty( $content ) || ! is_string( $content ) ) return (string) $content;
 
         if ( ! preg_match_all( '/<img[^>]+src=[\'"]([^\'"]+)[\'"][^>]*>/i', $content, $matches ) ) {
             return $content; 
@@ -115,12 +140,10 @@ class WDM_Importer {
 
         foreach ( $urls as $url ) {
             if ( strpos( $url, $old_domain ) !== false ) {
-                
                 $clean_url = strtok( $url, '?' );
-                
                 $attachment_id = $this->media_handler->sideload_image( $clean_url, $post_id );
                 
-                if ( ! is_wp_error( $attachment_id ) && ! is_array( $attachment_id ) ) {
+                if ( ! is_wp_error( $attachment_id ) && is_int( $attachment_id ) ) {
                     $new_url = wp_get_attachment_url( $attachment_id );
                     if ( $new_url ) {
                         $content = str_replace( $url, $new_url, $content );
@@ -128,7 +151,6 @@ class WDM_Importer {
                 }
             }
         }
-
         return $content;
     }
 }
